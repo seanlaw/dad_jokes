199149232b978bc27720b2ecb92caef38be3f81d
Sean Law <seanmylaw@gmail.com>
2021-07-16 17:14:21 -0400

    Minor change

--- github.sh
+++ github.sh
@@ -5,5 +5,6 @@ export 'BEARER_TOKEN'="$2"
 HOSTNAME="Github Actions"
 #DAD_JOKE=`python3 get_dad_jokes.py 2> /dev/null`
 DAD_JOKE=`python3 get_dad_jokes.py`
+DAD_JOKE="TEST"
 
 curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n\n$DAD_JOKE\n\n\"}" $MS_TEAMS_WEBHOOK -o /dev/null
