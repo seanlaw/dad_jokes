9c8724e142035c6203c6092941bb1e938dd58cb2
Sean Law <seanmylaw@gmail.com>
2021-07-16 16:38:55 -0400

    Remove error redirect

--- github.sh
+++ github.sh
@@ -3,6 +3,7 @@
 MS_TEAMS_WEBHOOK="$1"
 BEARER_TOKEN="$2"
 HOSTNAME="Github Actions"
-DAD_JOKE=`python3 get_dad_jokes.py 2> /dev/null`
+#DAD_JOKE=`python3 get_dad_jokes.py 2> /dev/null`
+DAD_JOKE=`python3 get_dad_jokes.py`
 
 curl -k -X POST -H 'Content-type: application/json' --data "{\"text\":\"Server: $HOSTNAME\n\n$DAD_JOKE\n\n\"}" $MS_TEAMS_WEBHOOK -o /dev/null
